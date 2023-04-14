61cb8070812639188945715c024a8481473b0f5c
cbigham25 <81174295+cbigham25@users.noreply.github.com>
2023-04-13 19:20:18 -0500

    added fakeName API and hooked it up

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 
-
+var heroName = document.querySelector('#name');
 var url = "https://chriscastle.com/proxy/index.php?:proxy:https://superheroapi.com/api/202045435900603/70";
 
 function logJSONData() {
@@ -15,7 +15,30 @@ function logJSONData() {
 
 logJSONData();
 
+var url = "https://chriscastle.com/proxy/index.php?:proxy:https://api.namefake.com/";
+
+function fetchFakeName() {
+    return new Promise((resolve, reject) => {
+        fetch(url, {}).then((response) => {
+            return response.json();
+        }).then((data) => {
+            const name = data.name;
+            resolve(name); // Resolve the Promise with the name
+        }).catch((error) => {
+            reject(error); // Reject the Promise with the error
+        });
+    });
+}
+
+
+fetchFakeName().then((name) => {
+    //TODO change this to be what ever we come up with for UI. 
+    heroName.textContent = name;
+}).catch((error) => {
+    console.error(error);
+});
+
 const generateButton = document.getElementById('clickButton');
-generateButton.addEventListener('click', function() {
+generateButton.addEventListener('click', function () {
     console.log('Thanks For Clicking!');
 });
