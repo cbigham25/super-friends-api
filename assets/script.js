c4e1e57e5202787ec50c5a94edefade22fd66d97
cbigham25 <81174295+cbigham25@users.noreply.github.com>
2023-04-17 20:35:16 -0500

    Added random logic for name generator

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-const names = ['Superman', 'Goku', 'Silver Surfer', 'Green Arrow', 'Black Canary', 'The Hulk', 'Spiderman', 'Wonder Woman'];
+//const names = ;
 const stats = ['Ultra Strength', 'Cat powers', 'super speed', 'fire powers', 'teleportation', 'time warping', 'material manipulation'];
 
 const nameEl = document.getElementById('names');
@@ -12,71 +12,110 @@ let superHeroUrl = "https://chriscastle.com/proxy/index.php?:proxy:https://super
 
 
 function logJSONData() {
-  fetch(superHeroUrl, {}).then((response) => {
-    return response.json();
-  }).then((data) => {
-    console.log(data);
-  });
+    fetch(superHeroUrl, {}).then((response) => {
+        return response.json();
+    }).then((data) => {
+        console.log(data);
+    });
 }
 
 logJSONData();
 
 function generateRandomSuperhero() {
-  const name = names[Math.floor(Math.random() * names.length)];
-  const power = stats[Math.floor(Math.random() * stats.length)];
-  const stat = Math.floor(Math.random() * 100) + 1;
-  const bio = `You are now named ${name}. You are a hero with ${power} abilities. Where will you use them first?`;
-  return { name, power, stat, bio };
+
+    let url = `https://chriscastle.com/proxy/index.php?:proxy:https://superheroapi.com/api/202045435900603/search/%20`;
+
+    fetch(url, {}).then((response) => {
+
+        return response.json();
+
+    }).then((data) => {
+
+        console.log(data);
+        let random1 = Math.floor(Math.random() * data.results.length) + 1;
+        let random2 = Math.floor(Math.random() * data.results.length) + 1;
+        let firstName = data.results[random1].name.split(" ")[0];
+        let lastName = data.results[random2].name.split(" ")[1];
+
+        const name = firstName + " " + lastName;
+        console.log(name);
+        const power = stats[Math.floor(Math.random() * stats.length)];
+        const stat = Math.floor(Math.random() * 100) + 1;
+        const bio = `You are now named ${name}. You are a hero with ${power} abilities. Where will you use them first?`;
+
+        const newData = {
+            heroName: name,
+            power: power,
+            stat: stat,
+            bio: bio
+        }
+
+        updateHTML(newData);
+
+    }).catch((error) => {
+        return error;
+    });
+
+
 }
 
 function updateHTML(data) {
-  nameEl.textContent = data.name;
-  statsEl.textContent = `${data.power}: ${data.stat}%`;
-  bioEl.textContent = data.bio;
+    nameEl.textContent = data.heroName;
+    statsEl.textContent = `${data.power}: ${data.stat}%`;
+    bioEl.textContent = data.bio;
 }
 
 function clearHTML() {
-  nameEl.textContent = '';
-  statsEl.textContent = '';
-  bioEl.textContent = '';
+    nameEl.textContent = '';
+    statsEl.textContent = '';
+    bioEl.textContent = '';
 }
 
 function saveData() {
-  const data = generateRandomSuperhero();
+    const data = generateRandomSuperhero();
     console.log(data);
 }
 
 generateButton.addEventListener('click', () => {
-  const data = generateRandomSuperhero();
-  updateHTML(data);
+    generateRandomSuperhero();
 });
 
 clearBtnEl.addEventListener('click', clearHTML);
 
 saveBtnEl.addEventListener('click', saveData);
 
-generateButton.addEventListener('click', function() {
+// generateButton.addEventListener('click', function () {
 
-let nameFakeurl = "https://chriscastle.com/proxy/index.php?:proxy:https://api.namefake.com/";
+//     let nameFakeurl = "https://chriscastle.com/proxy/index.php?:proxy:https://api.namefake.com/";
 
-function fetchFakeName() {
-    return new Promise((resolve, reject) => {
-        fetch(nameFakeurl, {}).then((response) => {
-            return response.json();
-        }).then((data) => {
-            const name = data.name;
-            resolve(name); // Resolve the Promise with the name
-        }).catch((error) => {
-            reject(error); // Reject the Promise with the error
-        });
-    });
+//     function fetchFakeName() {
+//         return new Promise((resolve, reject) => {
+//             fetch(nameFakeurl, {}).then((response) => {
+//                 return response.json();
+//             }).then((data) => {
+//                 const name = data.name;
+//                 resolve(name); // Resolve the Promise with the name
+//             }).catch((error) => {
+//                 reject(error); // Reject the Promise with the error
+//             });
+//         });
+//     }
+
+
+//     fetchFakeName().then((name) => {
+//         bioEl.textContent = "Real Name: " + name;
+//     }).catch((error) => {
+//         console.error(error);
+//     });
+
+// });
+
+
+//731
+function randomNameGeneration() {
+
+
 }
 
+randomNameGeneration();
 
-fetchFakeName().then((name) => {
-    bioEl.textContent = "Real Name: " + name;
-}).catch((error) => {   
-    console.error(error);
-});
-
-});
